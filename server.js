161a41a589c8de5a677713c9c0380a8238c3e876
zheng xue <2395035@edu.vaniercollege.qc.ca>
2023-12-01 17:38:35 -0500

    Update server.js

--- server.js
+++ server.js
@@ -75,6 +75,6 @@ const server = http.createServer((req,res)=>{
 
 });
 
-server.listen(3000,"localhost", ()=>{
-    console.log("Listening for requests on port 3000");
+server.listen(80,"localhost", ()=>{
+    console.log("Listening for requests on port 80");
 });
